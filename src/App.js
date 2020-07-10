df7398c701d1c9920ab2cec16929e6ba3fd4a2cb
burdier <miguelluis@outlook.com>
2020-07-10 10:39:22 -0400

    feat(component) remove variable that never used

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Header from './component/header'
 import Footer from './component/footer'
 import Body from './component/body'
